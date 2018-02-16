39bc5d9de150338256c375504ce56825a9a6759f
natashalballard <nbitcareer@gmail.com>
2018-02-15 21:12:00 -0500

    reverted back to working commit - excluded cloudinary

--- src/main/java/com/secureresume_wc/demo/MainController.java
+++ src/main/java/com/secureresume_wc/demo/MainController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
 import javax.validation.Valid;
+
 
 @Controller
 public class MainController {
@@ -18,27 +21,30 @@ public class MainController {
     @Autowired
     ResumeRepository resumeRepository;
 
+
+
+
     @RequestMapping("/")
     public String listResumes(Model model) {
         model.addAttribute("resumes", resumeRepository.findAll());
         return "index";
     }
-
-    @GetMapping("/add")
-    public String resumeForm(Model model) {
-        model.addAttribute("resume", new Resume());
-        return "resumeform";
-    }
-
-    @PostMapping("/process")
-    public String processForm(@Valid Resume resume, BindingResult result) {
-        if (result.hasErrors()) {
-            return "resumeform";
-        }
-        resumeRepository.save(resume);
-        return "redirect:/";
-    }
-
+//
+//    @GetMapping("/add")
+//    public String resumeForm(Model model) {
+//        model.addAttribute("resume", new Resume());
+//        return "resumeform";
+//    }
+//
+//    @PostMapping("/process")
+//    public String processForm(@Valid Resume resume, BindingResult result) {
+//        if (result.hasErrors()) {
+//            return "resumeform";
+//        }
+//        resumeRepository.save(resume);
+//        return "redirect:/";
+//    }
+//
 
     @RequestMapping("/display")
     public String showContactOnDisplay( Model model) {
@@ -50,25 +56,25 @@ public class MainController {
         model.addAttribute("references", referenceRepository.findAll());
         return "display";
     }
-
-    @RequestMapping("/detail/{id}")
-    public String showResume(@PathVariable("id") long id, Model model) {
-        model.addAttribute("resume", resumeRepository.findOne(id));
-        return "show";
-    }
-
-    @RequestMapping("/update/{id}")
-    public String updateResume(@PathVariable("id") long id, Model model){
-        model.addAttribute("resume", resumeRepository.findOne(id));
-        return "resumeform";
-    }
-
-    @RequestMapping("/delete/{id}")
-    public String delResume(@PathVariable("id") long id){
-        resumeRepository.delete(id);
-        return "redirect:/";
-    }
-
+//
+//    @RequestMapping("/detail/{id}")
+//    public String showResume(@PathVariable("id") long id, Model model) {
+//        model.addAttribute("resume", resumeRepository.findOne(id));
+//        return "show";
+//    }
+//
+//    @RequestMapping("/update/{id}")
+//    public String updateResume(@PathVariable("id") long id, Model model){
+//        model.addAttribute("resume", resumeRepository.findOne(id));
+//        return "resumeform";
+//    }
+//
+//    @RequestMapping("/delete/{id}")
+//    public String delResume(@PathVariable("id") long id){
+//        resumeRepository.delete(id);
+//        return "redirect:/";
+//    }
+//
 
 
 
